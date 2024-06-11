33fb0a7b3f49fa16a20f9f20ffdb90dec3fb91b7
gokul.menon <gokul.menon@free-now.com>
2024-06-11 17:11:08 +0200

    first commit

---
+++ blogs/blogs.js
@@ -0,0 +1,29 @@
+// blogs.js
+const blogs = [
+  {
+    heading: "Beyond Boundaries — Unveiling the Art of Scalability",
+    link: "scalability.html",
+    date: "January 1, 2022",
+    numberOfWords : 500 
+  },
+   {
+      heading: "Liquibase Tutorial",
+      link: "liquibase.html",
+      date: "January 1, 2022",
+      numberOfWords : 900 
+    },
+    {
+      heading: "Appium — Dealing with FLAG_SECURE enabled in application",
+      link: "appium.html",
+      date: "January 1, 2022",
+      numberOfWords : 200 
+    },
+    {
+      heading: "Heading - How do I edit $PATH (.bash_profile) on OSX?",
+      link: "bashprofile.html",
+      date: "January 1, 2022",
+      numberOfWords : 200 
+    },
+
+  ];
+  
